38ab8d7d45e92e192d7415cbb0512f10586f7d2f
Phumzakose <kosephumza@gmail.com>
2023-04-21 16:47:16 -0700

    added task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'e' && alphabets != 'q')
+		{
+			putchar(alphabets);
+		}
+	}
+
+	putchar('\n');
+
+return (0);
+
+}
