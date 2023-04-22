f5f87685474a8b562b4e2c846e50d4a58a9e3eef
Phumzakose <kosephumza@gmail.com>
2023-04-21 18:49:18 -0700

    modified task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (numbers = 0; numbers < 10; numbers++)
 	{
-		putchar((numbers % 10) + '0');
+		putchar('0' + numbers);
 	}
 
 	for (letters = 'a'; letters <= 'f'; letters++)
