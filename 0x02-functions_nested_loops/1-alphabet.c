7ccf34b8b2cc1ebaf9c892b6896284e2c92e02ac
Phumzakose <kosephumza@gmail.com>
2023-04-25 16:28:02 -0700

    added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char d;
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	
+}
+
