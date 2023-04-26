4a55036863c4b0c6e620c2555f60f394fab4a66e
Phumzakose <kosephumza@gmail.com>
2023-04-26 12:44:38 -0700

    modified task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,10 +25,10 @@ void times_table(void)
 			else if (k < 10 && j != 0)
 			{
 				_putchar(' ');
-				_putchar((res % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			else
-				_putchar((res % 10) + '0');
+				_putchar((k % 10) + '0');
 		}
 		_putchar('\n');
 	}
