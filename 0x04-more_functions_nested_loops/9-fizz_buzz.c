4581e93210f9fd5fab0958bd11faefe295391a9e
Phumzakose <kosephumza@gmail.com>
2023-04-28 17:06:32 -0700

    modified task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,11 +21,11 @@ int main(void)
 		}
 		else if (x % 3 == 0 &&x % 5 == 0)
 		{
-			printf("fizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("fizz");
+			printf("Fizz");
 		}
 		else if (x % 5 == 0)
 		{
