df2803ad25cf882af5ced05534b60ef8a12ea232
Phumzakose <kosephumza@gmail.com>
2023-04-28 17:34:56 -0700

    added task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints out a triangle
+ * @size: sets the size of the triangle
+ *
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = size; x > 0; x--)
+		{
+			for (y = 1; y <= size; y++)
+				if (y >= x)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
