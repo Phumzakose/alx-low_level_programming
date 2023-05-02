29a35a990cde075e9d0ddaf29c3953cdb3a47322
Phumzakose <kosephumza@gmail.com>
2023-05-02 15:59:40 -0700

    added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else if (i % 2 == 1)
+	{
+		for (k = (i - 1) / 2; k < i - 1; k++)
+		{
+			_putchar(str[k + 1]);
+		}
+	}
+	_putchar('\n');
+}
