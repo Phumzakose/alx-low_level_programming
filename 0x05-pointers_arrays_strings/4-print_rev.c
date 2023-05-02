28221452cee4337c7ea1b215ce2645e0c1c1387e
Phumzakose <kosephumza@gmail.com>
2023-05-02 14:48:43 -0700

    modified task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@ void print_rev(char *s)
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (j = i - 1; j >= 0; j--)
 	{
-		for (j = i - 1; j >= 0; j--)
-		{
-			_putchar(s[j]);
-		}
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
