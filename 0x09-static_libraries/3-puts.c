741566ad882d7c20f6a8f388292dc446441130a2
Phumzakose <kosephumza@gmail.com>
2023-05-12 15:43:58 -0700

    added task 1

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,16 +2,13 @@
 
 /**
  * _puts - prints a string to stdout
- * @s: pointer.
+ * @str: pointer.
  */
-void _puts(char *s)
+void _puts(char *str)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
