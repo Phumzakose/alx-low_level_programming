3023eb6acde1baa4658ff8571ccf4d7674d66c67
Phumzakose <kosephumza@gmail.com>
2023-05-16 14:21:38 -0700

    modified task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,20 +2,6 @@
 #include <stdlib.h>
 
 /**
- * strSize - return length of a string
- * @s: input
- * Return:  length of string
- */
-int strSize(char *s)
-{
-	int y;
-
-	for (y = 0; *s != '\0'; y++)
-		;
-	return (y);
-}
-
-/**
  * _strdup - copies input string in newly allocated memory
  * @str: input string
  * Return: NULL if str = NULL or pointer to duplicate string
@@ -23,22 +9,17 @@ int strSize(char *s)
 char *_strdup(char *str)
 {
 	char *c;
-	int i;
-	int size;
+	unsigned int i = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	size = strSize(str) + 1;
-	c = malloc(size * sizeof(char));
-	for (i = 0; i < size; i++)
+	c = malloc(sizeof(char) * i);
+	for (i = 0; str[i] != 0; i++)
 	{
-		if (c == NULL)
-		{
-			return (NULL);
-	}
 		c[i] = str[i];
 	}
+	c[i] = 0;
 	return (c);
 }
