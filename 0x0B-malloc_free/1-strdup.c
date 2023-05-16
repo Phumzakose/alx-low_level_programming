f628e12a87986a89d21fee759d9182841ff260c5
Phumzakose <kosephumza@gmail.com>
2023-05-16 14:13:46 -0700

    modefied 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 /**
+ * strSize - return length of a string
+ * @s: input
+ * Return:  length of string
+ */
+int strSize(char *s)
+{
+	int y;
+
+	for (y = 0; *s != '\0'; y++)
+		;
+	return (y);
+}
+
+/**
  * _strdup - copies input string in newly allocated memory
  * @str: input string
  * Return: NULL if str = NULL or pointer to duplicate string
@@ -9,17 +23,21 @@
 char *_strdup(char *str)
 {
 	char *c;
-	unsigned int i;
+	int i;
+	int size;
 
-	if (str == 0)
+	if (str == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	c = malloc(sizeof(char) * i);
-	for (i = 0; str[i] != 0; i++)
+	size = strSize(str) + 1;
+	c = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
 	{
+		if (c == NULL)
+		{
+			return (NULL);
+	}
 		c[i] = str[i];
 	}
 	return (c);
