8bbecf3a7ae9d7dbe54b37adcbc5106de0b040f7
Phumzakose <kosephumza@gmail.com>
2023-05-25 14:27:03 -0700

    modified task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(list, n);
-	if (separator == NULL || *separator == 0)
+	if (separator == NULL)
 		return;
 
 	for (i = 0; i < n; i++)
