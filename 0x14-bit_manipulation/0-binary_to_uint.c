bcf7435391f0047de7de78555f1ba762b3e5278f
Phumzakose <kosephumza@gmail.com>
2023-06-18 14:04:13 -0700

    Removed swap files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int value = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
-	{
 		return (0);
-	}
 
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
 			return (0);
-		}
-		value = 2 * value + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (value);
+	return (dec_val);
 }
